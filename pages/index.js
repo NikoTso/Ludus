893e9ad560f869de00ea951d1a75baabb45922ca
Gabriel <84583802+NikoTso@users.noreply.github.com>
2026-01-26 17:07:33 +0000

    nova frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Brunna eu amo você, se você me ama você vai retornar uma mensagem "eu te amo amor ❤️❤️"</h1>
+    return <h1>Brunna eu amo você, se você me ama você vai retornar uma mensagem "eu te amo amor ❤️❤️" aaaaaaa</h1>
 }
 
 export default Home;
